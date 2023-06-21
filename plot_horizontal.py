f2940e8df5691c1abd5d1e0a073798dba20ce5b5
Daniel Gratzl <danielgratzl99@gmail.com>
2023-06-21 22:09:48 +0200

    added panel webpage, humidity horiz

--- plot_horizontal.py
+++ plot_horizontal.py
@@ -1,12 +1,87 @@
 import xarray as xr
+import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 import os
+#matplotlib.use('Agg')
 
-def plot_horizontal(level, time):
+def create_fig_national_boundaries():
+    """
+    create a figure with national boundaries of central europe
+
+    Returns
+    -------
+    fig : figure handle
+    ax : axis handle
+    """
+    # Create a horizontal
+    fig = plt.figure(figsize=(8, 8))
+    ax = plt.axes(projection=ccrs.PlateCarree())
+
+    gl = ax.add_feature(cfeature.NaturalEarthFeature( 
+        category='cultural',
+        name='admin_0_countries',
+        scale='50m',
+        facecolor='none',
+        edgecolor='black',
+        linewidth = 0.2
+    ))
+    
+    lon_formatter = LongitudeFormatter(number_format='.0f')
+    lat_formatter = LatitudeFormatter(number_format='.0f')
+    ax.xaxis.set_major_formatter(lon_formatter)
+    ax.yaxis.set_major_formatter(lat_formatter)
+    
+    ax.set_extent([-4, 26, 31, 60])
+    
+    return fig, ax
+
+def plot_horizontal_geopotential(level, time, fig, ax):
+    """
+    plots geopotential as contour plot int an already existing figure
+
+    Parameters
+    ----------
+    level : height level [hpa]
+    time : current timestamp (dimension value of dataset)
+    fig : current figure handle
+    ax : current axis handle
+
+    Returns
+    -------
+    fig : current figure handle
+    ax : current axis handle
+
+    """
+    c = ds.sel(level=level, time=time).z.plot.contour(ax= ax, colors='k', linewidths=0.7,
+                                                      add_colorbar = False)
+    ax.clabel(c, c.levels, inline=True, fmt=fmt, fontsize=8)
+    
+    return fig, ax
+
+def plot_horizontal_temp(level, time, path):
+    """
+    plot geopotential and temp and save it
+
+    Parameters
+    ----------
+    level : height level [hpa]
+    time : current timestamp (dimension value of dataset)
+    path : path to current file incl. corresponding filename
+
+    Returns
+    -------
+    None.
+
+    """
+    fig, ax = create_fig_national_boundaries()
+    fig, ax = plot_horizontal_geopotential(level, time, fig, ax)
+    
+    cmap=plt.get_cmap('RdBu_r', 14)
     
     if level <= 300: # set min/maxs of colormap according to pressure level
         vmax = -25; vmin = -85
@@ -23,27 +98,49 @@ def plot_horizontal(level, time):
     else:
         vmax = 45; vmin = -15
     
-    # Create a horizontal
-    fig = plt.figure(figsize=(8, 6))
-    ax = plt.axes(projection=ccrs.PlateCarree())
+    mesh_t = ds.sel(level=level, time=time).t_c.plot.contourf(ax= ax, levels=np.arange(vmin,  vmax, 5), 
+                                                     cmap=cmap, vmin=vmin, vmax=vmax,
+                                                     add_colorbar = False)
+    ax.set_title(f"level = {level}, time = {str(time).split(':')[0]}")
+    plt.colorbar(mesh_t, ax=ax, label='temperature [°C]')
+    
+    #fig.savefig(path, dpi=100)
+    #plt.close()
+    plt.show()
+    
+def plot_horizontal_hum(level, time, path):
+    """
+    plot humidity&geopotential and save it
 
-    ax.add_feature(cfeature.NaturalEarthFeature( 
-        category='cultural',
-        name='admin_0_countries',
-        scale='50m',
-        facecolor='none',
-        edgecolor='black',
-        linewidth = 0.2
-    ))
-    ax.set_extent([-4, 26, 31, 60])
+    Parameters
+    ----------
+    level : height level [hpa]
+    time : current timestamp (dimension value of dataset)
+    path : path to current file incl. corresponding filename
+
+    Returns
+    -------
+    None.
+
+    """
+    fig, ax = create_fig_national_boundaries()
+    fig, ax = plot_horizontal_geopotential(level, time, fig, ax)    
     
-    cmap=plt.get_cmap('RdBu_r', 14)
+    # Apply the masks to the data variable
+    masked_data = xr.where(ds['r'] > 90, 2, xr.where(ds['r'] > 75, 1, np.nan))
     
-    c = ds.sel(level=level, time=time).z.plot.contour(ax= ax, colors='k', linewidths=0.7)
-    ax.clabel(c, c.levels, inline=True, fmt=fmt, fontsize=8)
-    ds.sel(level=level, time=time).t_c.plot.contourf(ax= ax, levels=np.arange(vmin,  vmax, 5), cmap=cmap, vmin=vmin, vmax=vmax)
+    # Create the contourf plot using xarray's plot function
+    mesh = masked_data.sel(level=level, time=time).plot.contourf(levels=[0, 1, 2], 
+                                                                 colors=['#7EDF7F', '#249527'], 
+                                                                 add_colorbar=False, 
+                                                                 ax=ax)
+    cbar = plt.colorbar(mesh, ax=ax, shrink=0.5, label='relative Humidity [%]')
+    cbar.set_ticks([0.5, 1.5])
+    cbar.set_ticklabels(['> 75', '> 90'])
+    ax.set_title(f"level = {level}hPa, time = {str(time).split(':')[0]}")
+    fig.savefig(path, dpi=100)
+    plt.close()
     
-    return fig, ax
 
 def fmt(x):
     """
@@ -65,24 +162,33 @@ def fmt(x):
 
 # Load ERA5 data from NetCDF file
 ds = xr.open_dataset(r'C:\Users\Surface Pro\OneDrive\Dokumente\Uni\Programmieren_test_git\era5_data_may_v2.nc')
-ds = ds.assign(t_c = ds["t"] - 273.15)
+ds = ds.assign(t_c = ds["t"] - 273.15) # temp in °C
 
-levels = ds.level.values # hpa 100, 200, 300, 400, 500, 600, 700,
-times = ds.time.values[0:5]# '2023-04-22T00-00-00' #timestamp
+# extract time and level dimensions from dataset
+levels = ds.level.values[0:1]
+times = ds.time.values[0:1]
 
+# loop over timestamps: one folder for each timestamp
 for time in times:
     time_str = pd.Timestamp(time).strftime("%Y%m%d_%H") # convert time to str for saving
-    try:
-        os.mkdir(f"../era5horiz_plots/{time_str}/")
-        current_dir = f"../era5horiz_plots/{time_str}/"
-    except FileExistsError:
-        current_dir = f"../era5horiz_plots/{time_str}/"
+    
+    # if directory for images does not yet exist, create it
+    if not os.path.exists("../era5horiz/"): 
+        os.mkdir("../era5horiz/") 
+    if not os.path.exists(f"../era5horiz/{time_str}/"):
+        os.mkdir(f"../era5horiz/{time_str}/")
+    current_dir = f"../era5horiz/{time_str}/"  
          
+    # plot each level extra
     for level in levels:
-        my_file = current_dir + f'{time_str}_{level}_era5_horiz.png'
-        if os.path.exists(my_file):
+        path_temp = current_dir + f'{time_str}_{level}_horiz_temp.png'
+        # if files already exist, don't create new ones
+        #if os.path.exists(path_temp): 
+        #    continue
+        plot_horizontal_temp(level, time, path_temp)
+        
+    for level in levels:
+        path_hum = current_dir + f'{time_str}_{level}_horiz_hum.png'
+        if os.path.exists(path_hum):
             continue
-            
-        fig, ax = plot_horizontal(level, time)
-        fig.savefig(my_file, dpi=400)
-        plt.close()
+        plot_horizontal_hum(level, time, path_hum)
--- plot_vertical_w_e.py
+++ plot_vertical_w_e.py
@@ -10,18 +10,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+import os
+import pandas as pd
 
-# Load ERA5 data from NetCDF file
-ds = xr.open_dataset(
-    r'C:\Users\Timm\PycharmProjects\SciProg\era5-3d-visualisation\era5_data_may.nc')
-
-def plot_vertical_we(latitude, time):
+def plot_vertical_we_temp(latitude, time, path):
 
     # Create a horizontal
     fig, ax = plt.subplots(figsize=(8, 6))
 
-    ds.sel(latitude=latitude, time=time).t.plot.contour(ax=ax, colors='k')
-    ds.sel(latitude=latitude, time=time).t.plot.contourf(ax=ax, levels=10, cmap='coolwarm')
+    ds.sel(latitude=latitude, time=time).t_c.plot.contour(ax=ax, colors='k')
+    ds.sel(latitude=latitude, time=time).t_c.plot.contourf(ax=ax, levels=10, cmap='coolwarm')
 
     lon = ds.sel(latitude=latitude, time=time).longitude
     lvl = ds.sel(latitude=latitude, time=time).level
@@ -40,8 +38,29 @@ def plot_vertical_we(latitude, time):
     ax.invert_yaxis()
 
     plt.yscale('log')
-    plt.show()
+    fig.savefig(path, dpi=400)
+    plt.close()
 
-lon = 40
-t = '2023-05-08T00-00-00'
-plot_vertical_we(lon, t)
+
+ds = xr.open_dataset(
+    r'C:\Users\Surface Pro\OneDrive\Dokumente\Uni\Programmieren_test_git\era5_data_may_v2.nc')
+ds = ds.assign(t_c = ds["t"] - 273.15)
+
+lats = ds.latitude.values[0::8]
+times = ds.time.values[0:5]
+
+for time in times:
+    time_str = pd.Timestamp(time).strftime("%Y%m%d_%H") # convert time to str for saving
+    if not os.path.exists("../era5vert_w_e/"):
+        os.mkdir("../era5vert_w_e/") 
+    if not os.path.exists(f"../era5vert_w_e/{time_str}/"):
+        os.mkdir(f"../era5vert_w_e/{time_str}/")
+    
+    current_dir = f"../era5vert_w_e/{time_str}/"
+    
+    for lat in lats:
+        path_temp = current_dir + f'{time_str}_{lat}_vert_w_e_temp.png'
+        if os.path.exists(path_temp):
+            continue
+        plot_vertical_we_temp(lat, time, path_temp)
+        
